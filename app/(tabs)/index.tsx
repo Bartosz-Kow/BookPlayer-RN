d4b15b1f767103da9528440b7de7407f91022104
Bartosz <bartoszkow03@gmail.com>
2024-10-15 18:53:48 +0200

    feat: add loading indicator for book list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,14 +7,17 @@ import { ScrollView, FlatList } from "react-native";
 import { fetchAllBooks } from "@/components/API/getBookList";
 import BookItem from "@/components/HomeComponents/BookItem";
 import CategoryChips from "@/components/HomeComponents/CategoryChips";
+import { ActivityIndicator } from "react-native-paper";
 
 const Index: React.FC = () => {
   const [booksData, setBooksData] = useState<any>({});
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const loadBooks = async () => {
       const results = await fetchAllBooks();
       setBooksData(results);
+      setIsLoading(false);
     };
 
     loadBooks();
@@ -37,51 +40,63 @@ const Index: React.FC = () => {
         <AuthorList />
         <CategoryChips />
         <DonateImage />
-        <StyledText className="font-sniglet text-center color-text-primary text-base">
-          Wolne książki na cały rok do słuchania
-        </StyledText>
-        <FlatList
-          data={booksData.wolneKsiazki}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.title}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ paddingHorizontal: 10 }}
-          horizontal
-        />
-        <StyledText className="font-sniglet text-center color-text-primary text-base">
-          Antologia Poetki Zagłady
-        </StyledText>
-        <FlatList
-          data={booksData.poetkiZaglady}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.title}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ paddingHorizontal: 10 }}
-          horizontal
-        />
-        <StyledText className="font-sniglet text-center color-text-primary text-base">
-          Baśnie, bajki, bajeczki
-        </StyledText>
 
-        <FlatList
-          data={booksData.bajkiBajeczki}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.title}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ paddingHorizontal: 10 }}
-          horizontal
-        />
-        <StyledText className="font-sniglet text-center color-text-primary text-base">
-          Biblioteczka antyczna
-        </StyledText>
-        <FlatList
-          data={booksData.biblioteczkaAntyczna}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.title}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ paddingHorizontal: 10 }}
-          horizontal
-        />
+        {isLoading ? (
+          <ActivityIndicator
+            animating={true}
+            size="large"
+            color="#CDE7BE"
+            style={{ marginTop: 20 }}
+          />
+        ) : (
+          <>
+            <StyledText className="font-sniglet text-center color-text-primary text-base">
+              Wolne książki na cały rok do słuchania
+            </StyledText>
+            <FlatList
+              data={booksData.wolneKsiazki}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.title}
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{ paddingHorizontal: 10 }}
+              horizontal
+            />
+            <StyledText className="font-sniglet text-center color-text-primary text-base">
+              Antologia Poetki Zagłady
+            </StyledText>
+            <FlatList
+              data={booksData.poetkiZaglady}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.title}
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{ paddingHorizontal: 10 }}
+              horizontal
+            />
+            <StyledText className="font-sniglet text-center color-text-primary text-base">
+              Baśnie, bajki, bajeczki
+            </StyledText>
+
+            <FlatList
+              data={booksData.bajkiBajeczki}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.title}
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{ paddingHorizontal: 10 }}
+              horizontal
+            />
+            <StyledText className="font-sniglet text-center color-text-primary text-base">
+              Biblioteczka antyczna
+            </StyledText>
+            <FlatList
+              data={booksData.biblioteczkaAntyczna}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.title}
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{ paddingHorizontal: 10 }}
+              horizontal
+            />
+          </>
+        )}
       </ScrollView>
     </StyledView>
   );
