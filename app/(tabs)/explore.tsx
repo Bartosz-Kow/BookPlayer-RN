42d1179ee43d2197c50ae5a0a3dcecf67daa7419
Bartosz <bartoszkow03@gmail.com>
2024-10-28 19:12:23 +0100

    Add styled searchbar

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,8 +1,28 @@
 import React from "react";
 import { StyledView } from "@/components/styledComponents";
+import { Searchbar } from "react-native-paper";
 
 const Explore = () => {
-  return <StyledView className="bg-background flex-1"></StyledView>;
+  const [searchQuery, setSearchQuery] = React.useState("");
+  return (
+    <StyledView className="bg-background flex-1">
+      <StyledView className="mt-10 px-5">
+        <Searchbar
+          placeholder="Szukaj audiobooków i książek"
+          onChangeText={setSearchQuery}
+          value={searchQuery}
+          style={{
+            backgroundColor: "#181A1A",
+            borderWidth: 1,
+            borderColor: "#CDE7BE",
+          }}
+          iconColor="#5F5F5F"
+          inputStyle={{ color: "#5F5F5F" }}
+          placeholderTextColor={"#5F5F5F"}
+        />
+      </StyledView>
+    </StyledView>
+  );
 };
 
 export default Explore;
