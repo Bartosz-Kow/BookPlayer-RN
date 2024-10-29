752b1bef47b51e688400dfc0ee24ec4141368745
Bartosz <bartoszkow03@gmail.com>
2024-10-29 16:46:05 +0100

    Add styles to ExploreBook component

--- components/ExploreComponents/ExploreBook.tsx
+++ components/ExploreComponents/ExploreBook.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
-import { StyledText } from "../styledComponents";
+import { Image } from "react-native";
+import { StyledText, StyledView, StyledOpacity } from "../styledComponents";
 
 interface ExploreBookItemProps {
   title: string;
@@ -16,44 +16,27 @@ const ExploreBookItem: React.FC<ExploreBookItemProps> = ({
   onPress,
 }) => {
   return (
-    <TouchableOpacity style={styles.container} onPress={onPress}>
-      <Image source={{ uri: cover }} style={styles.coverImage} />
-      <View style={styles.infoContainer}>
-        <Text style={styles.title}>{title}</Text>
+    <StyledOpacity
+      className="flex-row p-3 mb-4 rounded-lg bg-[#232323]"
+      onPress={onPress}
+    >
+      <Image
+        source={{ uri: cover }}
+        style={{ width: 60, height: 90, borderRadius: 5 }}
+      />
+      <StyledView className="flex-1 ml-3 justify-center">
+        <StyledText className="text-lg font-candal text-button-primary">
+          {title}
+        </StyledText>
         <StyledText
           numberOfLines={2}
           className="text-left mb-1 font-baskervill text-[#595353] text-sm max-w-[120px]"
         >
           {author}
         </StyledText>
-      </View>
-    </TouchableOpacity>
+      </StyledView>
+    </StyledOpacity>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    padding: 10,
-    marginBottom: 15,
-    borderRadius: 8,
-    backgroundColor: "#232323",
-  },
-  coverImage: {
-    width: 60,
-    height: 90,
-    borderRadius: 5,
-  },
-  infoContainer: {
-    flex: 1,
-    marginLeft: 10,
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 16,
-    fontWeight: "bold",
-    color: "#CDE7BE",
-  },
-});
 
 export default ExploreBookItem;
