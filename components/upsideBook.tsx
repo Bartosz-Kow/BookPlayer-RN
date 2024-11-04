f360a03ce255fdeef628f9a09fc8d13854643e52
Bartosz <bartoszkow03@gmail.com>
2024-11-04 16:51:16 +0100

    Add favorite toggle with color change for heart icon in UpsideBook component

--- components/upsideBook.tsx
+++ components/upsideBook.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { StyleSheet, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 import { useRouter } from "expo-router";
 import { Icon } from "react-native-paper";
 import { StyledOpacity, StyledView } from "./styledComponents";
 
-const UpsideBook = () => {
+const UpsideBook: React.FC = () => {
+  const [isFavorite, setFavorite] = useState<boolean>(false);
   const router = useRouter();
 
   return (
@@ -15,9 +15,15 @@ const UpsideBook = () => {
       >
         <Icon source="arrow-left-circle-outline" size={40} color="#CDE7BE" />
       </StyledOpacity>
-
-      <StyledOpacity className="w-10 h-10 items-center justify-center">
-        <Icon source="heart-circle-outline" size={40} color="#CDE7BE" />
+      <StyledOpacity
+        className="w-10 h-10 items-center justify-center"
+        onPress={() => setFavorite((prev) => !prev)}
+      >
+        <Icon
+          source="heart-circle-outline"
+          size={40}
+          color={isFavorite ? "red" : "#CDE7BE"}
+        />
       </StyledOpacity>
     </StyledView>
   );
